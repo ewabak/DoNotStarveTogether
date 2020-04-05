f7075aad32e0fefc8e72b48b3ecbd790e28312e2
ewabak <32634853+ewabak@users.noreply.github.com>
2020-04-05 22:09:33 +0200

    Initial commit

---
+++ 158558781/Cluster_5/Master/modoverrides.lua
@@ -0,0 +1,15 @@
+return {
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=false,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-632082897"]={ configuration_options={  }, enabled=true } 
+}
